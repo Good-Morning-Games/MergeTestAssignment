bf7b7ab222811d550e716ea7cd5f92a731c418aa
Ivan Yakimov <legendthechaos@gmail.com>
2023-09-22 20:04:29 +0600

    Initial Commit

---
+++ MergeUnityProject/Assets/Scripts/RandomCardSpawner.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class RandomCardSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GridSpawner gridSpawner;
+        
+    [SerializeField]
+    private GameObject cardPrefab;
+        
+    [Button]
+    private void SpawnRandomCard()
+    {
+        if (!gridSpawner.HasEmptyGridPositions())
+        {
+            Debug.LogError("No empty grid positions!");
+            return;
+        }
+        
+        var gridSize = gridSpawner.GridSize;
+
+        var maxX = gridSize.x;
+        var maxY = gridSize.y;
+            
+        Vector2Int randomGridPosition;
+            
+        do
+        {
+            var randomX = Random.Range(0, maxX);
+            var randomY = Random.Range(0, maxY);
+                
+            randomGridPosition = new Vector2Int(randomX, randomY);
+        } while (gridSpawner.HasCardAtGridPosition(randomGridPosition));
+            
+        gridSpawner.SpawnObjectAtGridPosition(cardPrefab, randomGridPosition);
+    }
+}
